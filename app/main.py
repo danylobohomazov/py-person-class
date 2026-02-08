a3fd689b79e8c5326482dd090ab54a3e3d58cafa
Danylo <danil.bogomazov1356@gmail.com>
2026-02-08 21:13:02 +0200

    Create class Person and func create_person_list

--- app/main.py
+++ app/main.py
@@ -1,8 +1,25 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        Person.people[name] = self
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    result_list = [Person(person["name"], person["age"]) for person in people]
+    for person in people:
+        need_person = find_person(result_list, person["name"])
+        if "wife" in person and person["wife"] is not None:
+            need_person.wife = Person.people[person["wife"]]
+        elif "husband" in person and person["husband"] is not None:
+            need_person.husband = Person.people[person["husband"]]
+    return result_list
+
+
+def find_person(persons: list[Person], name: str) -> (Person, None):
+    for person in persons:
+        if person.name == name:
+            return person
+    return None
